c333626bd3d0e943bd2e55aa173d5f1415dd56c7
CourtneyFradreck <courtaga@gmail.com>
2024-01-16 23:18:15 +0200

    changed var to let

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let game = ["Rock", "Paper", "Scissors"];
 let player = prompt("Choose between Rock, Paper or Scissors");
 alert("You chose " + player);
 
-var randomIndex = Math.floor(Math.random() * game.length); // a number from 0 to 2
-var randomItem = game[randomIndex]; // rock, paper or scissors
+let computer = Math.floor(Math.random() * game.length); // a number from 0 to 2
+let randomItem = game[computer]; // rock, paper or scissors
 alert("Computer chose " + randomItem); // prints the random item
