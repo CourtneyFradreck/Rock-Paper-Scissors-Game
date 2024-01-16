a3c9d18ff447dd0f480817a5f69cdc15fe4e3197
CourtneyFradreck <courtaga@gmail.com>
2024-01-16 23:15:49 +0200

    added the random function for the computer to randomly choose rock, paper or scissors

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 alert("Hey there!! Let's Play a Game");
 let game = ["Rock", "Paper", "Scissors"];
 let player = prompt("Choose between Rock, Paper or Scissors");
+alert("You chose " + player);
+
+var randomIndex = Math.floor(Math.random() * game.length); // a number from 0 to 2
+var randomItem = game[randomIndex]; // rock, paper or scissors
+alert("Computer chose " + randomItem); // prints the random item
