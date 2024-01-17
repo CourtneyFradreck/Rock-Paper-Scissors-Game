0c289918fe133f005317e86f4585dd7866dcb107
CourtneyFradreck <courtaga@gmail.com>
2024-01-17 16:03:50 +0200

    added comments to explain how the random module works

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let game = ["Rock", "Paper", "Scissors"];
 let player = prompt("Choose between Rock, Paper or Scissors");
 alert("You chose " + player);
 
-let computer = Math.floor(Math.random() * game.length); // a number from 0 to 2
-let randomItem = game[computer]; // rock, paper or scissors
+let computer = Math.floor(Math.random() * game.length); // computer's index..e.g number 0, 1, 2
+let randomItem = game[computer]; // rock, paper or scissors....computer's random choice
 alert("Computer chose " + randomItem); // prints the random item
 
 if (player === computer) {
