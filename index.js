e0e351d4de90da0cd75324e2613137e5f1761b20
courtaga@gmail.com <courtaga@gmail.com>
2025-01-15 16:18:34 +0200

    added documentation

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//function to get computer's choice
 function getComputerChoice(){
   switch ( Math.floor(Math.random() * 3)) {
     case 0:
@@ -9,16 +11,23 @@ function getComputerChoice(){
   }
 }
 
+//function to get human choice
 function getHumanChoice(){
   let HumanChoice = prompt("Chose between rock paper and scissors");
   HumanChoice = HumanChoice.toLowerCase();
   return HumanChoice;
 }
 
+
+//initialize score variables
 let HumanScore = 0;
 let ComputerScore = 0;
 
+
+//function to play a round
 function playRound(HumanChoice, ComputerChoice){
+
+  //conditional statements to determine the winner
   if (HumanChoice === ComputerChoice) {
     console.log("Its a tie.");
   }else if(HumanChoice === "rock" && ComputerChoice === "scissors" || HumanChoice === "scissors" && ComputerChoice === "paper" || HumanChoice === "paper" && ComputerChoice === "rock"
@@ -35,7 +44,7 @@ const HumanSelection = getHumanChoice();
 //const ComputerSelection = getComputerChoice();
 
 
-
+//function to play the game
 function playGame(){
   const ComputerSelection = getComputerChoice();
   playRound(HumanSelection, ComputerSelection);
