c0e0aba8fd00ec2939f44d971e6a453b4d071898
courtaga@gmail.com <courtaga@gmail.com>
2025-01-15 16:12:34 +0200

    refactor game logic to use functions for player and computer choices

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-function playGame() {
-  alert("Hey there!! Let's Play a Game");
-  let game = ["Rock", "Paper", "Scissors"];
-  let player = prompt("Choose between Rock, Paper or Scissors");
-  while (player !== "Rock" && player !== "Paper" && player !== "Scissors") {
-    alert("Invalid choice. Please choose between Rock, Paper or Scissors");
-    player = prompt("Choose between Rock, Paper or Scissors");
+function getComputerChoice(){
+  switch ( Math.floor(Math.random() * 3)) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
   }
-  alert("You chose " + player);
-
-  let computer = Math.floor(Math.random() * game.length); // computer's index..e.g number 0, 1, 2
-  let randomItem = game[computer]; // rock, paper or scissors....computer's random choice
-  alert("Computer chose " + randomItem); // prints the random item
-  document.write("You chose " + player + "<br>");
-  document.write("Computer chose " + randomItem + "<br>");
-
-  if (player === randomItem) {
-    document.write(
-      "It's a Draw! Both You and The Computer Chose The Same Item<br>"
-    );
-  } else if (player === "Rock" && randomItem === "Paper") {
-    document.write("Computer Won!<br>");
-  } else if (player === "Rock" && randomItem === "Scissors") {
-    document.write("You Won!<br>");
-  } else if (player === "Paper" && randomItem === "Rock") {
-    document.write("You Won!<br>");
-  } else if (player === "Paper" && randomItem === "Scissors") {
-    document.write("Computer Won!<br>");
-  } else if (player === "Scissors" && randomItem === "Rock") {
-    document.write("Computer Won!<br>");
-  } else if (player === "Scissors" && randomItem === "Paper") {
-    document.write("You Won!<br>");
-  }
-
-  const answer = prompt("Do you want to play again? (y/n)");
-
-  if (answer === "y") {
-    return playGame();
-  }
-
-  return;
 }
+
+function getHumanChoice(){
+  let HumanChoice = prompt("Chose between rock paper and scissors");
+  HumanChoice = HumanChoice.toLowerCase();
+  return HumanChoice;
+}
+
+let HumanScore = 0;
+let ComputerScore = 0;
+
+function playRound(HumanChoice, ComputerChoice){
+  if (HumanChoice === ComputerChoice) {
+    console.log("Its a tie.");
+  }else if(HumanChoice === "rock" && ComputerChoice === "scissors" || HumanChoice === "scissors" && ComputerChoice === "paper" || HumanChoice === "paper" && ComputerChoice === "rock"
+  ){
+    console.log(`You win ${HumanChoice} beats ${ComputerChoice}`);
+    HumanScore++;
+  }else{
+    console.log(`You lose ${ComputerChoice} beats ${HumanChoice}`);
+    ComputerScore++;
+  }
+
+}
+const HumanSelection = getHumanChoice();
+//const ComputerSelection = getComputerChoice();
+
+
+
+function playGame(){
+  const ComputerSelection = getComputerChoice();
+  playRound(HumanSelection, ComputerSelection);
+
+}
+
 playGame();
